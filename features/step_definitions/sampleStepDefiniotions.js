12d1075d651315eb655797b4e793e4a16493c558
Paulo Luan <pauloluan.inova@gmail.com>
2016-04-29 10:56:09 -0300

    first commit

---
+++ features/step_definitions/sampleStepDefiniotions.js
@@ -0,0 +1,30 @@
+module.exports = function() {
+
+    this.Given(/^I am on the Cucumber.js Github repository$/, function(callback) {
+        // Express the regexp above with the code you wish you had.
+        // `this` is set to a new this.World instance.
+        // i.e. you may use this.browser to execute the step:
+        this.visit('https://raw.githubusercontent.com/cucumber/cucumber-js/master/README.md', callback);
+
+        // The callback is passed to visit() so that when the job's finished, the next step can
+        // be executed by Cucumber.
+    });
+
+    this.When(/^I go to the README file$/, function(callback) {
+        // Express the regexp above with the code you wish you had. Call callback() at the end
+        // of the step, or callback.pending() if the step is not yet implemented:
+
+        callback(null, 'pending');
+    });
+
+    this.Then(/^I should see "(.*)" as the page title$/, function(title, callback) {
+        // matching groups are passed as parameters to the step definition
+
+        var pageTitle = this.browser.text('title');
+        if (title === pageTitle) {
+            callback();
+        } else {
+            callback(new Error("Expected to be on page with title " + title));
+        }
+    });
+};
